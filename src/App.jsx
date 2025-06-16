cc78d10017be5db84f07cfb9434e84abeff5bb5e
Eudald2000 <eudald08210@gmail.com>
2025-06-16 12:43:57 +0200

    turnos, nuevo tablero,

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Header } from './components/Header'
+import { Tablero } from './components/Tablero'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+  const [tablero, setTablero] = useState(
+    Array(7)
+      .fill(null)
+      .map(() => Array(8).fill(null))
+  )
+  const TURNOS = { 1: '🔵', 2: '🔴' }
+  const [turnoActual, setTurnoActual] = useState(1)
+
+  function handleClick (rowIndex, colIndex) {
+    if (tablero[rowIndex][colIndex]) return
+
+    const nuevoTablero = [...tablero]
+    nuevoTablero[rowIndex][colIndex] = TURNOS[turnoActual]
+    setTablero(nuevoTablero)
+    setTurnoActual((prev) => (prev === 1 ? 2 : 1))
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <main>
+        <div className="board">
+          {tablero.map((row, rowIndex) => (
+            <div className="row" key={rowIndex}>
+              {row.map((cell, colIndex) => (
+                <div
+                  onClick={() => handleClick(rowIndex, colIndex)}
+                  className="cell"
+                  key={colIndex}
+                >
+                  {cell === TURNOS[1] && (
+                    <img src="/4-En-Raya/circuloAzul.png" alt="Ficha azul" />
+                  )}
+                  {cell === TURNOS[2] && (
+                    <img src="/4-En-Raya/circuloRojo.png" alt="Ficha roja" />
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </main>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export const Header = ({}) => {
+  return (
+    <header>
+    <div className='title'>4 en Raya</div>
+    <div className='score'>
+      🔵 scorePlayer1 - scorePlayer2 🔴
+    </div>
+  </header>
+  )
+}
